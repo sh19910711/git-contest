795f3c1c43d98468ad2a4eb5dee9ad7968410183
Hiroyuki Sano <sh19910711@gmail.com>
2013-09-22 10:03:58 +0900

    Improve git-contest-init

--- git-contest.gemspec
+++ git-contest.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "mechanize"
   spec.add_dependency "nokogiri"
   spec.add_dependency "trollop"
+  spec.add_dependency "highline"
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
--- lib/git/contest/git.rb
+++ lib/git/contest/git.rb
@@ -1,5 +1,11 @@
 def git_do(*args)
+  puts "git #{args.join(' ')}"
   return `git #{args.join(' ')}`
+end
+
+def git_do_no_echo(*args)
+  puts "git #{args.join(' ')}"
+  system "git #{args.join(' ')} >/dev/null 2>&1"
 end
 
 def git_contest_is_initialized
@@ -11,10 +17,15 @@ def git_contest_has_master_configured
   master != "" && git_local_branches().include?(master)
 end
 
+def git_local_branch_exists(branch_name)
+  git_local_branches().include?(branch_name)
+end
+
 def git_local_branches
   cmd_ret = git_do 'branch --no-color'
   cmd_ret.lines.map {|line|
     line.gsub(/^[*]?\s*/, '').gsub(/\s*$/, '')
   }
 end
+
 
