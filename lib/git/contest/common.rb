0923566199e859a9974608d9dcd7f506f267e82a
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-16 17:22:39 +0900

    Add init_home into git/contest/common

--- lib/git/contest/common.rb
+++ lib/git/contest/common.rb
@@ -12,13 +12,23 @@ GIT_CONTEST_CONFIG_DEFAULT = GIT_CONTEST_HOME_DEFAULT + '/config.yml'
 
 def init
   init_global
+  init_home
 end
 
 def init_global
-  $GIT_CONTEST_HOME   = ENV['GIT_CONTEST_HOME'] || GIT_CONTEST_HOME_DEFAULT
-  $GIT_CONTEST_CONFIG = ENV['GIT_CONTEST_CONFIG'] || GIT_CONTEST_CONFIG_DEFAULT
+  $GIT_CONTEST_HOME   = File.expand_path(ENV['GIT_CONTEST_HOME'] || GIT_CONTEST_HOME_DEFAULT)
+  $GIT_CONTEST_CONFIG = File.expand_path(ENV['GIT_CONTEST_CONFIG'] || GIT_CONTEST_CONFIG_DEFAULT)
   $MASTER = git_do 'config --get git.contest.branch.master'
   $PREFIX = git_do 'config --get git.contest.branch.prefix'
+end
+
+def init_home
+  if ! FileTest.directory? $GIT_CONTEST_HOME
+    FileUtils.mkdir $GIT_CONTEST_HOME
+  end
+  if ! FileTest.exists? $GIT_CONTEST_CONFIG
+    FileUtils.touch $GIT_CONTEST_CONFIG
+  end
 end
 
 def get_config
