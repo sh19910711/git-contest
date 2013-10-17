1c5595ade0e57f12d25d71b8c663549097207055
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-18 03:58:50 +0900

    Add spec T006_001_002

--- spec/bin/t006_config_spec.rb
+++ spec/bin/t006_config_spec.rb
@@ -55,6 +55,43 @@ describe "T006: Config Test" do
 
     end
 
+    describe "002: source" do
+
+      before do
+        Dir.mkdir '001'
+        Dir.chdir '001'
+        File.open "ac.cpp", "w" do |file|
+          file.write "ac-code"
+        end
+        File.open "wa.d", "w" do |file|
+          file.write "wa-code"
+        end
+        File.open "tle.go", "w" do |file|
+          file.write "tle-code"
+        end
+        bin_exec "init --defaults"
+      end
+
+      it "001: ac.*" do
+        ENV['GIT_CONTEST_CONFIG'] = get_path('/mock/t006/001/002/001/config.yml')
+        ret = bin_exec "submit test_dummy -c 100 -p A"
+        ret.include?('Dummy 100A: Accepted')
+      end
+
+      it "002: wa.*" do
+        ENV['GIT_CONTEST_CONFIG'] = get_path('/mock/t006/001/002/002/config.yml')
+        ret = bin_exec "submit test_dummy -c 100 -p A"
+        ret.include?('Dummy 100A: Wrong Answer')
+      end
+
+      it "003: tle.*" do
+        ENV['GIT_CONTEST_CONFIG'] = get_path('/mock/t006/001/002/003/config.yml')
+        ret = bin_exec "submit test_dummy -c 100 -p A"
+        ret.include?('Dummy 100A: Time Limit Exceeded')
+      end
+
+    end
+
   end
 
 end
