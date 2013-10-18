ba8f8fb0fee1c0f274cd19c095b98df77b7d0c9a
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-18 18:20:35 +0900

    Edit Spec T009

--- spec/bin/t009_git_contest_init_spec.rb
+++ spec/bin/t009_git_contest_init_spec.rb
@@ -4,16 +4,18 @@ require "spec_helper"
 
 describe "T009: git-contest-init" do
 
-  it "001: init -> init" do
-    abort "to check: display already initalized error"
-  end
+  describe "001: --force" do
+    it "001: init -> init" do
+      abort "to check: display already initalized error"
+    end
 
-  it "002: init -> init -f" do
-    abort "to check: does not display already initalized error"
-  end
+    it "002: init -> init -f -> init --force" do
+      abort "to check: does not display already initalized error"
+    end
 
-  it "003: init -f -> init -f" do
-    abort "to check: does not display already initalized error"
+    it "003: init -f -> init -f -> init --force" do
+      abort "to check: does not display already initalized error"
+    end
   end
 
 end
