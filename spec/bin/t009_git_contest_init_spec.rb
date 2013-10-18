1eaf3cd081ff29cac930c4dca6a8c2a2f77ef4c2
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-18 18:10:27 +0900

    Add Spec T009 for git-contest-init

---
+++ spec/bin/t009_git_contest_init_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+# Don't forget --defaults option
+
+describe "T009: git-contest-init" do
+
+  it "001: init -> init" do
+    abort "to check: display already initalized error"
+  end
+
+  it "002: init -> init -f" do
+    abort "to check: does not display already initalized error"
+  end
+
+  it "003: init -f -> init -f" do
+    abort "to check: does not display already initalized error"
+  end
+
+end
+
