9e3ce254feaab55740624f35c0970b280be8342c
Hiroyuki Sano <sh19910711@gmail.com>
2014-11-05 23:55:27 +0900

    git-contest-finish: use FinishCommand class on tests

--- spec/bin/t008_git_contest_finish_spec.rb
+++ spec/bin/t008_git_contest_finish_spec.rb
@@ -3,12 +3,19 @@ require "spec_helper"
 # Do not forget --no-edit option
 
 describe "T008: git-contest-finish" do
+
+  def call_main(args)
+    cli = CommandLine::MainCommand.new(args)
+    cli.init
+    cli
+  end
+
   context "A001: --keep" do
-    it "001: init -> start -> empty-commits -> finish" do
-      bin_exec "init --defaults"
-      bin_exec "start branch1"
+    it "001: init -> start -> empty-commits -> finish", :current => true do
+      expect { call_main(["init", "--defaults"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["start", "branch1"]).run }.to output(/.*/).to_stdout
       Git.do "commit --allow-empty -m 'this is commit'"
-      bin_exec "finish --no-edit"
+      expect { call_main(["finish", "--no-edit"]).run }.to output(/.*/).to_stdout
       ret1 = Git.do "branch"
       ret_log1 = Git.do "log --oneline master"
       expect(ret1.split(/\s+/)).not_to include /branch1/
@@ -16,10 +23,10 @@ describe "T008: git-contest-finish" do
     end
 
     it "002: init -> start -> empty-commits -> finish --keep" do
-      bin_exec "init --defaults"
-      bin_exec "start branch1"
+      expect { call_main(["init", "--defaults"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["start", "branch1"]).run }.to output(/.*/).to_stdout
       Git.do "commit --allow-empty -m 'this is commit'"
-      bin_exec "finish --no-edit --keep"
+      expect { call_main(["finish", "--no-edit", "--keep"]).run }.to output(/.*/).to_stdout
       ret1 = Git.do "branch"
       ret_log1 = Git.do "log --oneline master"
       expect(ret1.split(/\s+/)).to include /branch1/
@@ -27,10 +34,10 @@ describe "T008: git-contest-finish" do
     end
 
     it "003: init -> start -> empty-commits -> finish -k" do
-      bin_exec "init --defaults"
-      bin_exec "start branch1"
+      expect { call_main(["init", "--defaults"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["start", "branch1"]).run }.to output(/.*/).to_stdout
       Git.do "commit --allow-empty -m 'this is commit'"
-      bin_exec "finish --no-edit -k"
+      expect { call_main(["finish", "--no-edit", "-k"]).run }.to output(/.*/).to_stdout
       ret1 = Git.do "branch"
       ret_log1 = Git.do "log --oneline master"
       expect(ret1.split(/\s+/)).to include /branch1/
@@ -42,23 +49,23 @@ describe "T008: git-contest-finish" do
   context "A002: --rebase" do
     it "001: init -> start -> empty-commits -> finish --rebase" do
       # create branches: branch1(normal) -> branch2(rebase) -> branch3(normal)
-      bin_exec "init --defaults"
-      bin_exec "start branch1"
-      10.times {|x|
+      expect { call_main(["init", "--defaults"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["start", "branch1"]).run }.to output(/.*/).to_stdout
+      3.times {|x|
         name = "test-1.#{x}"
         FileUtils.touch name
         Git.do "add #{name}"
         Git.do "commit -m 'Add #{name}'"
       }
-      bin_exec "start branch2"
-      10.times {|x|
+      expect { call_main(["start", "branch2"]).run }.to output(/.*/).to_stdout
+      3.times {|x|
         name = "test-2.#{x}"
         FileUtils.touch name
         Git.do "add #{name}"
         Git.do "commit -m 'Add #{name}'"
       }
-      bin_exec "start branch3"
-      10.times {|x|
+      expect { call_main(["start", "branch3"]).run }.to output(/.*/).to_stdout
+      3.times {|x|
         name = "test-3.#{x}"
         FileUtils.touch name
         Git.do "add #{name}"
@@ -66,9 +73,9 @@ describe "T008: git-contest-finish" do
       }
       # finish branches
       ret_branch_1 = Git.do "branch"
-      bin_exec "finish --no-edit branch1"
-      bin_exec "finish --no-edit --rebase branch2"
-      bin_exec "finish --no-edit branch3"
+      expect { call_main(["finish", "--no-edit", "branch1"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["finish", "--no-edit", "--rebase", "branch2"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["finish", "--no-edit", "branch3"]).run }.to output(/.*/).to_stdout
       ret_branch_2 = Git.do "branch"
       ret_log = Git.do "log --oneline"
       expect(ret_branch_1.split(/\s+/)).to include /branch1/
@@ -86,12 +93,12 @@ describe "T008: git-contest-finish" do
     # TODO: recheck
     it "001: init -> start -> trigger merge error -> finish --force-delete" do
       # make conflict
-      bin_exec "init --defaults"
+      expect { call_main(["init", "--defaults"]).run }.to output(/.*/).to_stdout
       FileUtils.touch "test.txt"
       Git.do "add test.txt"
       Git.do "commit -m 'Add test.txt'"
-      bin_exec "start branch1"
-      bin_exec "start branch2"
+      expect { call_main(["start", "branch1"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["start", "branch2"]).run }.to output(/.*/).to_stdout
       Git.do "checkout contest/branch1"
       File.open "test.txt", "w" do |file|
         file.write "test1"
@@ -105,8 +112,8 @@ describe "T008: git-contest-finish" do
       Git.do "add test.txt"
       Git.do "commit -m 'Edit test.txt @ branch2'"
       # finish
-      bin_exec "finish --no-edit branch1"
-      bin_exec "finish --no-edit --force-delete branch2"
+      expect { call_main(["finish", "--no-edit", "branch1"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["finish", "--no-edit", "--force-delete", "branch2"]).run }.to output(/.*/).to_stdout
       ret_branch = Git.do "branch"
       expect(ret_branch.split(/\s+/)).not_to include /contest\/branch1/
       expect(ret_branch.split(/\s+/)).not_to include /contest\/branch2/
@@ -115,15 +122,15 @@ describe "T008: git-contest-finish" do
 
   context "A004: --squash" do
     it "001: init -> start -> empty-commits -> finish --squash" do
-      bin_exec "init --defaults"
-      bin_exec "start branch1"
-      10.times do |x|
+      expect { call_main(["init", "--defaults"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["start", "branch1"]).run }.to output(/.*/).to_stdout
+      3.times do |x|
         filename = "test#{x}.txt"
         FileUtils.touch filename
         Git.do "add #{filename}"
         Git.do "commit -m 'this is commit #{x}'"
       end
-      bin_exec "finish --no-edit --squash branch1"
+      expect { call_main(["finish", "--no-edit", "--squash", "branch1"]).run }.to output(/.*/).to_stdout
       ret_log1 = Git.do "log --oneline"
       ret_branch1 = Git.do "branch"
       expect(ret_branch1.split(/\s+/)).not_to include /branch1/
@@ -136,26 +143,26 @@ describe "T008: git-contest-finish" do
     before do
       Dir.mkdir "src"
       Dir.chdir "src"
-      bin_exec "init --defaults"
-      bin_exec "start branch1"
-      10.times {|x| Git.do "commit --allow-empty -m 'this is commit #{x}'" }
+      expect { call_main(["init", "--defaults"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["start", "branch1"]).run }.to output(/.*/).to_stdout
+      3.times {|x| Git.do "commit --allow-empty -m 'this is commit #{x}'" }
       Dir.chdir ".."
-      Git.do "clone src dest"
+      Git.do "clone --single-branch -b master src dest"
       Dir.chdir "dest"
     end
 
     it "001: init -> start -> clone -> checkout@dest -> empty-commits@dest -> finish@dest" do
       Git.do "checkout -b master origin/master"
-      bin_exec "init --defaults"
-      bin_exec "start --fetch branch1"
-      bin_exec "finish --no-edit --fetch branch1"
+      expect { call_main(["init", "--defaults"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["start", "--fetch", "branch1"]).run }.to output(/.*/).to_stdout
+      expect { call_main(["finish", "--no-edit", "--fetch", "branch1"]).run }.to output(/.*/).to_stdout
       ret_branch2 = Git.do "branch"
       Dir.chdir ".."
       Dir.chdir "src"
       ret_branch1 = Git.do "branch"
       Git.do "checkout master"
       expect(ret_branch1.split(/\s+/)).to include /branch1/
-      expect(ret_branch2.split(/\s+/)).not_to include /branch1/
+      expect(ret_branch2.split(/\s+/)).to_not include /branch1/
     end
   end
 end
