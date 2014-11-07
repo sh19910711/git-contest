f51699235abd5cc1ae162a2bb1e626b2fd5d74a4
Hiroyuki Sano <sh19910711@gmail.com>
2014-11-07 10:34:09 +0900

    Use command classes (branching test)

--- spec/bin/t005_git_contest_branching_spec.rb
+++ spec/bin/t005_git_contest_branching_spec.rb
@@ -1,7 +1,15 @@
 require "spec_helper"
 
 describe "T005: branching" do
-  context "A001" do
+
+  def call_main(args)
+    cli = CommandLine::MainCommand.new(args)
+    cli.init
+    cli
+  end
+
+  context "create config file" do
+
     before do
       ENV['GIT_CONTEST_CONFIG'] = "#{@temp_dir}/home/config.yml"
       File.open ENV['GIT_CONTEST_CONFIG'], "w" do |file|
@@ -15,56 +23,68 @@ EOF
       end
     end
 
-    context 'B001' do 
-      it '001: init -> start -> submit -> submit -> finish' do
-        Dir.mkdir 'test1'
-        Dir.chdir 'test1'
+    context "$ git contest init", :current => true do
 
-        # Init
-        bin_exec "init --defaults"
-        expect(Git.current_branch).to eq 'master'
-        ret = Git.do "log --oneline"
-        expect(ret).to include 'Initial commit'
+      before { expect {call_main(["init", "--defaults"]).run}.to output("").to_stdout }
+      it { expect(Git.current_branch).to eq "master" }
 
-        # Start
-        bin_exec "start contest1"
-        expect(Git.current_branch).to eq 'contest/contest1'
+      context "$ git contest start contest1" do
 
-        # Edit.1
-        File.open 'main.c', 'w' do |file|
-          file.write 'wa-code'
-        end
+        before { expect {call_main(["start", "contest1"]).run}.to output(/contest1/).to_stdout }
+        it { expect(Git.current_branch).to eq "contest/contest1" }
 
-        # Submit.1
-        bin_exec "submit test_dummy -c 1000 -p A"
-        ret = Git.do "log --oneline"
-        expect(ret).to include 'Dummy 1000A: Wrong Answer'
+        context "edit main.c as wrong" do
 
-        ret = Git.do "ls-files"
-        expect(ret).to include 'main.c'
+          before do
+            File.open 'main.c', 'w' do |file|
+              file.write 'wa-code'
+            end
+          end
 
-        # Edit.2 fixed
-        File.open 'main.c', 'w' do |file|
-          file.write 'ac-code'
-        end
+          context "$ git contest submit" do
 
-        # Submit.2
-        bin_exec "submit test_dummy -c 1000 -p A"
-        ret = Git.do "log --oneline"
-        expect(ret).to include 'Dummy 1000A: Accepted'
+            before { expect {call_main(["submit", "test_dummy", "-c", "1000", "-p", "A"]).run}.to output(/Wrong Answer/).to_stdout }
 
-        ret = Git.do "ls-files"
-        expect(ret).to include 'main.c'
+            context "fix main.c" do
 
-        # Finish
-        bin_exec "finish --no-edit"
-        expect(Git.current_branch).to eq 'master'
+              before do
+                File.open 'main.c', 'w' do |file|
+                  file.write 'ac-code'
+                end
+              end
 
-        ret = Git.do "log --oneline"
-        expect(ret).to include 'Dummy 1000A: Wrong Answer'
-        expect(ret).to include 'Dummy 1000A: Accepted'
-      end
-    end
-  end
-end
+              context "$ git contest submit (resubmit)" do
+
+                before { expect {call_main(["submit", "test_dummy", "-c", "1000", "-p", "A"]).run}.to output(/Accepted/).to_stdout }
+
+                context "$ git contest finish" do
+
+                  before { expect {call_main(["finish", "--no-edit"]).run}.to output(/contest1/).to_stdout }
+                  it { expect(Git.current_branch).to eq "master" }
+
+                  context "$ git log" do
+
+                    subject { Git.do "log --oneline" }
+                    it { should include "Dummy 1000A: Wrong Answer" }
+                    it { should include "Dummy 1000A: Accepted" }
+
+                  end # git log
+
+                end # git contest finish
+
+              end # git contest submit (resubmit)
+
+            end # fix main.c
+
+          end # git contest sbumit
+
+        end # edit main.c
+
+      end # git contest start contest1
+
+    end # git contest init
+
+  end # create config file
+
+end # brancing
 
